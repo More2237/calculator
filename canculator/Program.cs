67ffab3bce9993ff3a603e59434403e36631bfd5
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-24 16:02:58 +0500

    first commit

---
+++ canculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace canculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var a = int.Parse(ReadLine()); 
+          var b = int.Parse(ReadLine());
+          var operation = ReadLine();
+          int result;
+
+          switch (operation)
+          {
+              case "+":
+                  result = a + b;
+                  WriteLine(result);
+                  break;
+              case "-":
+                  result = a - b;
+                  WriteLine(result);
+                  break;
+              case "*":
+                  result = a * b;
+                  WriteLine(result);
+                  break;
+              case "/":
+                  if (operation == "/")
+                      result = a / b;
+                  else
+                  {
+                      WriteLine("cant divide by 0");
+                  }
+                  break;
+                  
+              default:
+                  WriteLine("oshibka");
+                  break;
+          }
+        }
+    }
+}
